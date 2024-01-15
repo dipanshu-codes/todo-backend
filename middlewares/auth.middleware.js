16317747359d50f56a78132cd89613fe84ef61de
dipanshu-codes <dipanshu.codes@gmail.com>
2024-01-15 17:49:10 +0530

    ⚙️Modified middleware for auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,20 +6,19 @@ require("dotenv").config();
 const SECRET = process.env.SECRET || undefined;
 
 function verifyToken(req, res, next) {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ "msg": "Unauthorized..." });
+    return res.status(401).json({ msg: "Unauthorized..." });
   }
 
-  jwt.verify(token, SECRET, function(error, decoded) {
-    if (error) {
-      return res.status(401).json({ "msg": "Unauthorized..." });
-    }
+  const decoded = jwt.verify(token, SECRET)
+  if (!decoded) {
+    return res.status(401).json({ msg: "Unauthorized..." });
+  }
 
-    req.body.userId = decoded.userId;
-    next();
-  });
+  req.body.userId = decoded.userId;
+  next();
 }
 
 module.exports = verifyToken;
